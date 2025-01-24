55692062e1ab473f7da60c67c690990cedfc09ba
AlexanderFasching <alexander.fasching@stud.fh-campuswien.ac.at>
2025-01-25 00:13:38 +0100

    ka

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/VAR/",
+  base: "/VAR/", // Replace <REPOSITORY_NAME> with your GitHub repo name
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"], // Ensure `three` is bundled separately
+        },
+      },
+    },
+  },
   optimizeDeps: {
-    include: ["three"], // Force Vite to pre-bundle 'three'
+    include: ["three", "three/examples/jsm/loaders/GLTFLoader.js"], // Pre-bundle dependencies
   },
 });
